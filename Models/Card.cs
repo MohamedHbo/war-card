9e4d14ca68098ac53b40d46e731a030b2c85a267
Mohamed Sayed <109354903+MohamedHbo@users.noreply.github.com>
2024-08-12 20:41:09 +0300

    Add files via upload

---
+++ Models/Card.cs
@@ -0,0 +1,11 @@
+﻿namespace war.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Person Person { get; set; }
+    }
+}
---
+++ Models/CardDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace war.Models
+{
+    public class CardDbContext:DbContext
+    {
+        public CardDbContext(DbContextOptions<CardDbContext>option):base(option) {
+        
+        
+        
+        }
+        
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        
+        
+    }
+}
