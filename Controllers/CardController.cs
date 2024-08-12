09d719ca2d5b0db00d34c766667ad3fff24a1aef
Mohamed Sayed <109354903+MohamedHbo@users.noreply.github.com>
2024-08-12 20:46:16 +0300

    Add files via upload

---
+++ Controllers/CardController.cs
@@ -0,0 +1,232 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.IO;
+using System.Linq;
+using war.Models;
+using war.Models.Repositories;
+using war.Models.ViewModels;
+
+namespace war.Controllers
+{
+    public class CardController : Controller
+    {
+        private readonly ICardRepositories<Card> cardRepositories;
+        private readonly ICardRepositories<Person> personRepositories;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public CardController(ICardRepositories<Card> cardRepositories, ICardRepositories<Person> personRepositories, IWebHostEnvironment hostingEnvironment)
+        {
+            this.cardRepositories = cardRepositories;
+            this.personRepositories = personRepositories;
+            this._hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET: CardController/Index
+        public ActionResult Index(int? personId)
+        {
+            
+            var cards = cardRepositories.List();
+
+            
+            var persons = personRepositories.List().ToList();
+            ViewBag.Persons = new SelectList(persons, "Id", "Name", personId);
+
+            
+            if (personId.HasValue)
+            {
+                cards = cards.Where(c => c.Person.Id == personId.Value).ToList();
+            }
+
+           
+            var cardViewModels = cards.Select(card => new CardPersonViewModel
+            {
+                CardId = card.Id,
+                CardName = card.Name,
+                CardDescription = card.Description,
+                CardImage = card.Image,
+                PersonName = card.Person.Name
+            }).ToList();
+
+            return View(cardViewModels);
+        }
+
+
+       
+        public ActionResult Details(int id)
+        {
+            var card = cardRepositories.Find(id);
+            var model = new CardPersonViewModel
+            {
+                CardId = card.Id,
+                CardName = card.Name,
+                CardDescription = card.Description,
+                CardImage = card.Image,
+                PersonName = card.Person.Name
+            };
+            return View(model);
+        }
+
+        // GET: CardController/Create
+        public ActionResult Create()
+        {
+            var model = new CardPersonViewModel
+            {
+                Persons = personRepositories.List().ToList()
+            };
+            return View(model);
+        }
+
+        // POST: CardController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CardPersonViewModel model)
+        {
+            try
+            {
+                if (model.File != null && model.File.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + model.File.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        model.File.CopyTo(fileStream);
+                    }
+                    model.CardImage = uniqueFileName;
+                }
+
+                var person = personRepositories.Find(model.PersonId);
+                var newCard = new Card
+                {
+                    Name = model.CardName,
+                    Description = model.CardDescription,
+                    Image = model.CardImage,
+                    Person = person
+                };
+
+                cardRepositories.Add(newCard);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        // GET: CardController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var card = cardRepositories.Find(id);
+            var persons = personRepositories.List().ToList();
+
+            if (persons == null || !persons.Any())
+            {
+                ViewData["ErrorMessage"] = "No persons available. Please add a person first.";
+                return View(new CardPersonViewModel { Persons = new List<Person>() });
+            }
+
+            var model = new CardPersonViewModel
+            {
+                CardId = card.Id,
+                CardName = card.Name,
+                CardDescription = card.Description,
+                CardImage = card.Image,
+                PersonId = card.Person.Id,
+                Persons = persons
+            };
+
+            // Log or debug the model to ensure it is correct
+            Console.WriteLine($"Model: {System.Text.Json.JsonSerializer.Serialize(model)}");
+
+            return View(model);
+        }
+
+
+
+
+
+        // POST: CardController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, CardPersonViewModel model)
+        {
+            try
+            {
+                var cardToUpdate = cardRepositories.Find(id);
+
+                if (model.File != null && model.File.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + model.File.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        model.File.CopyTo(fileStream);
+                    }
+                    model.CardImage = uniqueFileName;
+                }
+                else
+                {
+                    model.CardImage = cardToUpdate.Image;
+                }
+
+                cardToUpdate.Name = model.CardName;
+                cardToUpdate.Description = model.CardDescription;
+                cardToUpdate.Image = model.CardImage;
+                cardToUpdate.Person = personRepositories.Find(model.PersonId);
+
+                cardRepositories.Update(cardToUpdate);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        // GET: CardController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var card = cardRepositories.Find(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            // Debugging: Ensure the card object is populated correctly
+            Console.WriteLine($"Deleting Card: {card.Name}, {card.Description}, {card.Person?.Name}, {card.Image}");
+
+            return View(card);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                var card = cardRepositories.Find(id);
+                if (card == null)
+                {
+                    return NotFound();
+                }
+
+                cardRepositories.Delete(card);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+    }
+}
---
+++ Controllers/PersonController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using war.Models;
+using war.Models.Repositories;
+using war.Models.ViewModels;
+
+namespace war.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly ICardRepositories<Person> _personRepositories;
+
+        public PersonController(ICardRepositories<Person> personRepositories)
+        {
+            _personRepositories = personRepositories;
+        }
+
+        // GET: PersonController
+        public ActionResult Index()
+        {
+            var persons = _personRepositories.List();
+            return View(persons);
+        }
+
+        // GET: PersonController/Details/5
+        public ActionResult Details(int id)
+        {
+            var person = _personRepositories.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        // GET: PersonController/Create
+        public ActionResult Create()
+        {
+            var viewModel = new CardPersonViewModel
+            {
+                Persons = (List<Person>)_personRepositories.List()
+            };
+            return View(viewModel);
+        }
+
+        // POST: PersonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CardPersonViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var person = _personRepositories.Find(viewModel.PersonId);
+                if (person != null)
+                {
+                    viewModel.PersonName = person.Name;
+                    // Save the card information along with the person name
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            viewModel.Persons = (List<Person>)_personRepositories.List();
+            return View(viewModel);
+        }
+
+        // GET: PersonController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var person = _personRepositories.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        // POST: PersonController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _personRepositories.Update(id, person);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: PersonController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var person = _personRepositories.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        // POST: PersonController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                _personRepositories.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
