ac13550f8ea218797c9ed2c5e9ef878c319dc912
Mohamed Sayed <109354903+MohamedHbo@users.noreply.github.com>
2024-08-12 20:38:08 +0300

    Add files via upload

---
+++ ViewModels/CardPersonViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace war.Models.ViewModels
+{
+    public class CardPersonViewModel
+    {
+        public int CardId { get; set; }
+
+        [Required]
+        public string CardName { get; set; }
+
+        [Required]
+        public string CardDescription { get; set; }
+
+        public string CardImage { get; set; }
+
+        public int PersonId { get; set; }
+
+        public string PersonName { get; set; }
+
+        public IFormFile File { get; set; }
+        public List<Person> Persons { get; set; }
+    }
+}
